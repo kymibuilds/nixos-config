591ddd23c6d6178333e3e1f2e1b48bbf19a344bc
kymibuilds <kymibuilds@gmail.com>
2025-12-29 00:14:23 +0530

    chore: code janitoring, added hardware-config

--- configuration.nix
+++ configuration.nix
@@ -1,59 +1,56 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
 in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      (import "${home-manager}/nixos")
-    ];
+  # Imports & Home Manager
+  imports = [
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+  ];
 
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
   home-manager.backupFileExtension = "backup";
   home-manager.users.to_run = import ./home.nix;
 
-  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.nixPath = [ "nixos-config=/etc/nixos/configuration.nix" ];
 
-  # systemd boot
+  # Boot & Core System
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  time.timeZone = "Asia/Kolkata";
+  nixpkgs.config.allowUnfree = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-
-  # Configure network connections interactively with nmcli or nmtui.
-  networking.networkmanager.enable = true;
-  programs.xwayland.enable = true;
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
+  # Hardware & Performance
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.enableRedistributableFirmware = true;
+  boot.kernelParams = [
+    "i915.enable_dc=4"
+    "i915.enable_psr=1"
   ];
 
-  nix.nixPath = [ "nixos-config=/etc/nixos/configuration.nix" ];
+  # User Account
+  users.users.to_run = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [ tree ];
+  };
 
   security.sudo.extraConfig = ''
     Defaults env_keep += "NIX_PATH"
   '';
 
-  # Set your time zone.
-  time.timeZone = "Asia/Kolkata";
+  # Networking
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    config.common.default = "*";
-  };
-
+  # Power Management
   powerManagement.enable = true;
   powerManagement.powertop.enable = true;
-
   services.tlp.enable = true;
   services.tlp.settings = {
     CPU_SCALING_GOVERNOR_ON_AC = "performance";
@@ -63,146 +60,87 @@ in
     WIFI_PWR_ON_BAT = "on";
     USB_AUTOSUSPEND = "1";
   };
-  hardware.cpu.intel.updateMicrocode = true;
 
-  boot.kernelParams = [
-    "i915.enable_dc=4"
-    "i915.enable_psr=1"   # Panel Self Refresh
-  ];
+  # Wayland / Session
+  programs.xwayland.enable = true;
+  programs.niri.enable = true;
+  programs.gtklock.enable = true;
 
-  hardware.enableRedistributableFirmware = true;
-
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  # lightdm
   services.xserver = {
     enable = true;
     displayManager.lightdm.enable = true;
     displayManager.sessionCommands = ''
-     xwallpaper --zoom ~/rice/wallpapers/2.jpg
+      xwallpaper --zoom ~/rice/wallpapers/2.jpg
     '';
   };
 
-  programs.niri.enable = true;
-
-  programs.gtklock.enable = true;
-
+  # Idle & Locking behavior
+  systemd.user.services.swayidle = {
+    description = "Idle manager for Wayland";
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart =
+        "${pkgs.swayidle}/bin/swayidle -w before-sleep '${pkgs.gtklock}/bin/gtklock -m ${pkgs.gtklock-powerbar-module}/lib/gtklock/powerbar-module.so'";
+      Restart = "on-failure";
+    };
+  };
 
   services.logind.settings.Login = {
     HandleLidSwitch = "suspend";
     HandleLidSwitchDocked = "ignore";
   };
 
-  systemd.user.services.swayidle = {
-    description = "Idle manager for Wayland";
-    wantedBy = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.swayidle}/bin/swayidle -w before-sleep '${pkgs.gtklock}/bin/gtklock -m ${pkgs.gtklock-powerbar-module}/lib/gtklock/powerbar-module.so'";
-      Restart = "on-failure";
-    };
+  systemd.services."display-manager".serviceConfig.KillMode = "mixed";
+
+  # Portals
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
   };
 
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.to_run = {
-   isNormalUser = true;
-   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-   packages = with pkgs; [
-     tree
-   ];
-  };
-
+  # Applications
   programs.firefox.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
- environment.systemPackages = with pkgs; [
-   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   wget
-   git
-   networkmanager
-   bluez
-   bluez-tools
-
-   adwaita-icon-theme
-   dconf
-   glib
-   gsettings-desktop-schemas
-   papirus-icon-theme
-   colloid-gtk-theme
- ];
-
- programs.dconf.enable = true;
-
- environment.sessionVariables = lib.mkForce {
-  GTK_THEME = "Colloid-Dark";
-  NIX_PATH = "nixos-config=/etc/nixos/configuration.nix";
-  NIXOS_OZONE_WL = "1";
-  ELECTRON_OZONE_PLATFORM_HINT = "auto";
-
-  XDG_DATA_DIRS = [
-    "${pkgs.gsettings-desktop-schemas}/share"
-    "${pkgs.glib}/share"
+  programs.dconf.enable = true;
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    networkmanager
+    bluez
+    bluez-tools
+    adwaita-icon-theme
+    dconf
+    glib
+    gsettings-desktop-schemas
+    papirus-icon-theme
+    colloid-gtk-theme
   ];
-};
 
- fonts.packages = with pkgs; [
-   source-code-pro
-   jetbrains-mono
-   noto-fonts-color-emoji
-   noto-fonts-monochrome-emoji
- ];
+  # Environment Vars
+  environment.sessionVariables = lib.mkForce {
+    GTK_THEME = "Colloid-Dark";
+    NIX_PATH = "nixos-config=/etc/nixos/configuration.nix";
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    XDG_DATA_DIRS = [
+      "${pkgs.gsettings-desktop-schemas}/share"
+      "${pkgs.glib}/share"
+    ];
+  };
 
- systemd.services."display-manager".serviceConfig.KillMode = "mixed";
+  # Fonts
+  fonts.packages = with pkgs; [
+    source-code-pro
+    jetbrains-mono
+    fira-code
+    inter
+    noto-fonts-color-emoji
+  ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # allowed ports
-  networking.firewall.enable = false;
-
+  # State version
   system.stateVersion = "25.11";
 }
